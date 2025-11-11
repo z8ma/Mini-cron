7553f12dd82f9aad29ac09f0e15d35e6c474a8b7
choux <julien.choux@etu.u-paris.fr>
2025-11-11 22:17:02 +0100

    Initialisation des methode pour lire des argumants et des commandes

--- src/serialisation.c
+++ src/serialisation.c
@@ -32,3 +32,103 @@ int readtiming(int fd, struct timing *tbuf) {
     tbuf->hours = be32toh(tbuf->hours);
     return 0;
 }
+
+int readarguments(int fd, struct arguments *abuf) {
+    if (read(fd, abuf, sizeof(uint32_t)) < 0) return 1;
+
+    abuf->argc = be32toh(abuf->argc);
+    abuf->argv = malloc((abuf->argc) * sizeof(struct string));
+    for (size_t i = 0; i < abuf->argc; i++) {
+        if (readstring(fd, &abuf->argv[i]) < 0) return 1;
+    }
+
+    return 0;
+}
+
+void freearguments(struct arguments *abuf) {
+    for(int i = 0; i < abuf->argc; i++) {
+        freestring(&(abuf->argv[i]));
+    }
+    free((abuf)->argv);
+}
+
+void insertion_sort(char **arr, size_t n) {
+    for (size_t i = 1; i < n; i++) {
+        char *key = arr[i];
+        ssize_t j = i - 1;
+        while (j >= 0 && strcmp(arr[j], key) > 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int readcmd(char *filename, struct command *cbuf) {
+    struct stat st;
+    ssize_t fd;
+    char typename[2];
+    char path[PATH_MAX];
+    
+    if (lstat(filename, &st) < 0) return 1;
+    if ((st.st_mode & S_IFMT) != S_IFDIR) return 1;
+
+    snprintf(path, strlen(filename) + 6, "%s/type", filename);
+    fd = open(path, O_RDONLY);
+    if (fd < 0) return 1;
+    if (read(fd, typename, 2) < 0) return 1;
+    cbuf->type = (typename[0]<<8) + typename[1];
+    close(fd);
+
+    if (cbuf->type == SI_TYPE) {
+        snprintf(path, strlen(filename) + 6, "%s/argv", filename);
+        fd = open(path, O_RDONLY);
+        if (fd < 0) return 1;
+        int ret = readarguments(fd, &(cbuf->args));
+        close(fd);
+        return ret;
+    } else if (cbuf->type == SQ_TYPE) {
+        struct dirent *entry;
+        cbuf->nbcmds = 0;
+        char **names = NULL;
+
+        DIR *dirp = opendir(filename);
+        if (!dirp) return 1;
+
+        while ((entry = readdir(dirp))) {
+            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 && (entry->d_type == DT_DIR)) {
+                names = realloc(names, (cbuf->nbcmds + 1) * sizeof(char *));
+                names[cbuf->nbcmds++] = (entry->d_name);
+            }
+        }
+
+        if (cbuf->nbcmds == 0) return 1;
+        cbuf->cmd = malloc(cbuf->nbcmds * sizeof(struct command));
+        if (!cbuf->cmd) return 1;
+        insertion_sort(names, cbuf->nbcmds);
+
+        for (int i = 0; i < cbuf->nbcmds; i++) {
+            snprintf(path, strlen(filename) + strlen(names[i]) + 2, "%s/%s", filename, names[i]);
+            if (readcmd(path, (cbuf->cmd) + i) != 0) {
+                free(names);
+                closedir(dirp);
+                return 1;
+            }
+        }
+        free(names);
+        closedir(dirp);
+        return 0;
+    }
+    return 0;
+}
+
+void freecmd(struct command *cbuf) {
+    if (cbuf->type == SI_TYPE) {
+        freearguments(&(cbuf->args));
+    } else if (cbuf->type == SQ_TYPE) {
+        for(int i = 0; i < cbuf->nbcmds; i++) {
+            freecmd(&(cbuf->cmd[i]));
+        }
+        free(cbuf->cmd);
+    }
+}
