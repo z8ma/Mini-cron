45e49e04744a8ad5b1ff6e8610c07bef047a243f
Leny Sokou <reyleny7@gmail.com>
2025-11-17 16:43:46 +0100

    1ère version du démon

--- src/erraid.c
+++ src/erraid.c
@@ -6,6 +6,8 @@
 #include <getopt.h>
 #include <string.h>
 #include <errno.h>
+#include <dirent.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
@@ -42,6 +44,72 @@ int main(int argc, char *argv[])
         {
             perror("Ce dossier n'existe pas.");
             return 1;
+        }
+    }
+    mkdir("tasks", 0644);
+
+    pid_t p1 = fork();
+    if (p1 < 0)
+    {
+        exit(1);
+    }
+
+    if (p1 == 0)
+    {
+        pid_t parent_pid = getppid();
+        while (getppid() == parent_pid)
+        {
+            sleep(1);
+        }
+    }
+    else
+    {
+        exit(0);
+    }
+    DIR *dirp = opendir("tasks");
+    struct dirent *entry;
+    while (1)
+    {
+        while (entry = readdir(dirp))
+        {
+            if (entry->d_type == DT_DIR)
+            {
+                chdir(entry->d_name);
+                int fd_timing = open("timing", O_RDONLY);
+                struct timing *time;
+                if (readtiming(fd_timing, time))
+                {
+                    exit(1);
+                }
+                if (is_it_time(time))
+                {
+                    struct command *cmd;
+                    if (readcmd("cmd", cmd))
+                    {
+                        exit(1);
+                    }
+                    else
+                    {
+                        pid_t p2 = fork();
+                        if (p2 == 0)
+                        {
+                            int fd_out = open("stdout", O_WRONLY | O_CREAT | O_APPEND | O_EXCL, 0644);
+                            int fd_err = open("stderr", O_WRONLY | O_CREAT | O_APPEND | O_EXCL, 0644);
+                            int fd_exit_code = open("times-exitcodes", O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0644);
+                            dup2(fd_out, STDOUT_FILENO);
+                            dup2(fd_err, STDERR_FILENO);
+                            dup2(fd_exit_code, STDERR_FILENO);
+                            dup2(fd_exit_code, STDOUT_FILENO);
+                            executecmd(cmd);
+                            exit(0);
+                        }
+                        else
+                        {
+                            wait(NULL);
+                        }
+                    }
+                }
+            }
         }
     }
 }
