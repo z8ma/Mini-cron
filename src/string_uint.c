3df37a69da52779f9f1237af75316fd585fcbb20
Souhaile <souhaile@etu.u-paris.fr>
2025-12-12 15:32:05 +0100

    correction de conversion dans string_uint.c

--- src/string_uint.c
+++ src/string_uint.c
@@ -5,19 +5,26 @@
 #include <stdint.h>
 
 int readstring(int fd, struct string *sbuf) {
-    if (read(fd, &(sbuf->length), sizeof(uint32_t)) < 0) return 1;
 
-    sbuf->data = malloc(be32toh((sbuf->length) + 1) * sizeof(uint8_t));
+    uint32_t length_be;
+
+    if (read(fd, &length_be, sizeof(uint32_t)) < 0) return 1;
+    uint32_t host_length = be32toh(length_be);
+    sbuf->length = host_length;
+
+    sbuf->data = malloc((host_length + 1) * sizeof(uint8_t));
     if(!sbuf->data) return 1;
 
-    if (read(fd, sbuf->data, be32toh(sbuf->length) * sizeof(uint8_t)) < 0) return 1;
-    sbuf->data[be32toh(sbuf->length)] ='\0';
+    if (read(fd, sbuf->data, host_length * sizeof(uint8_t)) != (ssize_t)host_length * sizeof(uint8_t)) return 1;
+    sbuf->data[host_length] ='\0';
     return 0;
 }
 
 int writestring(int fd, struct string *sbuf) {
-    if (write(fd, &(sbuf->length), sizeof(uint32_t)) < 0) return 1;
-    if (write(fd, sbuf->data, be32toh(sbuf->length) * sizeof(uint8_t)) < 0) return 1;
+    uint32_t length_be = htobe32(sbuf->length);
+    if (write(fd, &length_be, sizeof(uint32_t)) != sizeof(uint32_t)) return 1;
+
+    if (write(fd, sbuf->data,sbuf->length * sizeof(uint8_t)) != (ssize_t)sbuf->length * sizeof(uint8_t)) return 1;
     return 0;
 }
 
