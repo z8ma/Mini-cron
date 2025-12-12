0f55409396731bbd3b4ce7e294891d3d0080c40d
Souhaile <souhaile@etu.u-paris.fr>
2025-12-12 19:28:35 +0100

    réglages de conversion dans is_it_time

--- src/timing.c
+++ src/timing.c
@@ -37,9 +37,9 @@ int is_it_time(struct timing *tbuf) {
     struct tm *now = localtime(&timestamp);
     if ((tbuf->daysofweek & (1<<(now->tm_wday))) == 0) {
         return 0;
-    } else if ((be32toh(tbuf->hours) & (1<<(now->tm_hour))) == 0) {
+    } else if ((tbuf->hours & (1<<(now->tm_hour))) == 0) {
         return 0;
-    } else if ((be64toh(tbuf->minutes) & (1ULL<<(now->tm_min))) == 0) {
+    } else if ((tbuf->minutes & (1ULL<<(now->tm_min))) == 0) {
        return 0; 
     }
     return 1;
