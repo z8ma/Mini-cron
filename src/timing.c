6defa6481ec8fd30223b2e6546dc1b820faa1c4b
SOKOU leny <leny.sokou@etu.u-paris.fr>
2025-12-14 21:04:57 +0100

    Modifier timing.c

--- src/timing.c
+++ src/timing.c
@@ -1,4 +1,5 @@
 #include "timing.h"
+#include <string_uint.h>
 
 #include <stdint.h>
 #include <endian.h>
@@ -43,4 +44,118 @@ int is_it_time(struct timing *tbuf) {
        return 0; 
     }
     return 1;
+}
+
+int timing_to_string(struct timing t, struct string *s) {
+    struct string comma = {1, (uint8_t*)","};
+    struct string space = {1, (uint8_t*)" "};
+    struct string star = {1, (uint8_t*)"*"};
+    struct string dash = {1, (uint8_t*)"-"};
+    int first;
+    int pred;
+    int duration;
+
+    if (t.minutes == 0) {
+        catstring(s, dash);
+    } else if ((t.minutes & 0xfffffffffffffff) == 0xfffffffffffffff) {
+        catstring(s, star);
+    } else {
+        first = 1;
+        pred = -1;
+        duration = 0;
+        for (int i = 0; i < 60; i++) {
+            if (t.minutes & (1ULL << i)) {
+                if (!first && i != 59) {
+                    if (pred != i - 1) {
+                        if (duration) {
+                            uint_to_string(pred, s);
+                            duration = 0;
+                        }
+                        catstring(s, comma);
+                    } else if(!duration) {
+                        duration = 1;
+                        catstring(s, dash);
+                    }
+                }
+                if (!duration || i == 59) {
+                    uint_to_string(i, s);
+                }
+                first = 0;
+                pred = i;
+            } else if (duration) {
+                duration = 0;
+                uint_to_string(pred, s);
+            }
+        }
+    }
+    catstring(s, space);
+    if (t.hours == 0) {
+        catstring(s, dash);
+    } else if ((t.hours & 0xffffff) == 0xffffff) {
+        catstring(s, star);
+    } else {
+        first = 1;
+        pred = -1;
+        duration = 0;
+        for (int i = 0; i < 24; i++) {
+            if (t.hours & (1u << i)) {
+                    if (!first && i != 23) {
+                    if (pred != i - 1) {
+                        if (duration) {
+                            uint_to_string(pred, s);
+                            duration = 0;
+                        }
+                        catstring(s, comma);
+                    } else if(!duration) {
+                        duration = 1;
+                        catstring(s, dash);
+                    }
+                }
+                if (!duration || i == 23) {
+                    uint_to_string(i, s);
+                }
+                first = 0;
+                pred = i;
+            } else if (duration) {
+                duration = 0;
+                uint_to_string(pred, s);
+            }
+        }
+    }
+    catstring(s, space);
+
+    if (t.daysofweek == 0) {
+        catstring(s, dash);
+    } else if ((t.daysofweek & 0x7f) == 0x7f) {
+        catstring(s, star);
+    } else {
+        first = 1;
+        pred = -1;
+        duration = 0;
+        for (int i = 0; i < 7; i++) {
+            if (t.daysofweek & (1 << i)) {
+                    if (!first && i != 6) {
+                    if (pred != i - 1) {
+                        if (duration) {
+                            uint_to_string(pred, s);
+                            duration = 0;
+                        }
+                        catstring(s, comma);
+                    } else if(!duration) {
+                        duration = 1;
+                        catstring(s, dash);
+                    }
+                }
+                if (!duration || i == 6) {
+                    uint_to_string(i, s);
+                }
+                first = 0;
+                pred = i;
+            } else if (duration) {
+                duration = 0;
+                uint_to_string(pred, s);
+            }
+        }
+    }
+    return 0;
 }
