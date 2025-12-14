78f56b9c1c6d765c0ae9cad84a8e1028a03cd448
SOKOU leny <leny.sokou@etu.u-paris.fr>
2025-12-14 21:03:56 +0100

    Modifier string_uint.c

--- src/string_uint.c
+++ src/string_uint.c
@@ -1,6 +1,7 @@
 #include "string_uint.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
 
@@ -26,5 +27,39 @@ int writestring(int fd, struct string *sbuf) {
 }
 
 void freestring(struct string *sbuf) {
-    free(sbuf->data);
+    if (sbuf->data != NULL) {
+        free(sbuf->data);
+    }
+}
+
+int catstring(struct string *newstring, struct string oldstring) {
+    if (newstring->length == 0) {
+        newstring->data = NULL;
+    }
+    newstring->length += oldstring.length;
+    uint8_t *tmp = realloc(newstring->data, (newstring->length + 1) * sizeof(uint8_t));
+    if (!tmp) return 1;
+    newstring->data = tmp;
+    for (int i = 0; i < oldstring.length; i++) {
+        newstring->data[newstring->length - oldstring.length + i] = oldstring.data[i];
+    }
+    newstring->data[newstring->length] = '\0';
+    return 0;
+}
+
+int uint_to_string(uint64_t n, struct string *s) {
+    if (!s) {
+        s->length = 0;
+        s->data = NULL;
+    }
+    uint32_t len = snprintf(NULL, 0,"%zu", n);
+    if (len < 0) return 1;
+
+    uint8_t *tmp = realloc(s->data ,(s->length + len + 1) * sizeof(uint8_t));
+    if (!tmp) return 1;
+
+    s->data = tmp;
+    snprintf((char*)(s->data + s->length), len + 1,"%zu", n);
+    s->length +=len;
+    return 0;
 }
