a01d85d2359b8e9a1303d1396ab6e33e3fa439ae
SOKOU leny <leny.sokou@etu.u-paris.fr>
2026-01-05 14:25:32 +0100

    Modifier command.c

--- src/command.c
+++ src/command.c
@@ -1,184 +1,247 @@
-#include "command.h"
-#include "arguments.h"
-#include "communication.h"
+#include "request.h"
+#include "reply.h"
+#include "task.h"
 #include "times_exitcodes.h"
+#include "string_uint.h"
 
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <dirent.h>
+#include <getopt.h>
 #include <string.h>
-#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <time.h>
 
+int handle_list_request(struct request req, struct reply *rbuf) {
+    rbuf->anstype = OK_ANSTYPE;
+    rbuf->content.list.nbtasks = 0;
 
-void insertion_sort(char **arr, size_t n) {
-    for (size_t i = 1; i < n; i++) {
-        char *key = arr[i];
-        ssize_t j = i - 1;
-        while (j >= 0 && strcmp(arr[j], key) > 0) {
-            arr[j + 1] = arr[j];
-            j--;
+    struct string *names = NULL;
+    char path_task[PATH_MAX];
+
+    DIR *dirp = opendir("tasks");
+    if (!dirp) return 1;
+
+    struct dirent *entry;
+    while ((entry = readdir(dirp))) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 && (entry->d_type == DT_DIR)) {
+            names = realloc(names, (rbuf->content.list.nbtasks + 1) * sizeof(struct string));
+            names[rbuf->content.list.nbtasks].length = strlen(entry->d_name);
+            names[rbuf->content.list.nbtasks].data = malloc(names[rbuf->content.list.nbtasks].length + 1);
+            strcpy((char *) names[rbuf->content.list.nbtasks].data, entry->d_name);
+            rbuf->content.list.nbtasks++;
         }
-        arr[j + 1] = key;
     }
+    closedir(dirp);
+    insertion_sort_strings(names, rbuf->content.list.nbtasks);
+    rbuf->content.list.tasks = malloc(rbuf->content.list.nbtasks * sizeof(struct task));
+    for (int i = 0; i < rbuf->content.list.nbtasks; i++) {
+        snprintf(path_task, sizeof(path_task), "tasks/%s", names[i].data);
+        string_to_uint64(names[i], &((rbuf->content.list.tasks + i)->taskid));
+        readtask_timing(path_task, &((rbuf->content.list.tasks + i)->task_timing));
+        readtask_command(path_task, &((rbuf->content.list.tasks + i)->task_command));
+        freestring(names + i);
+    }
+    
+    free(names);
+    return 0;
 }
 
-int readcmd_path(char *filename, struct command *cbuf) {
+int handle_creat_request(struct request req, struct reply *rbuf) {
+    uint64_t taskid_be = 0;
+    rbuf->anstype = OK_ANSTYPE;
+    int fd = open("last-taskid", O_RDWR | O_CREAT, 0644);
     struct stat st;
-    int fd;
-    char path[PATH_MAX];
-    uint16_t type_be;
-    
-    if (lstat(filename, &st) < 0) return 1;
-    if ((st.st_mode & S_IFMT) != S_IFDIR) return 1;
-
-    snprintf(path, strlen(filename) + 6, "%s/type", filename);
-    fd = open(path, O_RDONLY);
-    if (fd < 0) return 1;
-    if (read(fd, &type_be, sizeof(uint16_t)) < 0) return 1;
-    cbuf->type = be16toh(type_be);
+    if (fstat(fd, &st) < 0)
+        return 1;
+    if (st.st_size != 0) {
+        read(fd, &taskid_be, sizeof(uint64_t));
+    }
+    struct task task = {be64toh(taskid_be) + 1, req.content.cr.task_timing, {SI_TYPE, {req.content.cr.content.args}}};
+    char path_task[PATH_MAX];
+    snprintf(path_task, sizeof(path_task), "tasks/%zu/", task.taskid);
+    mkdirtask(path_task, &task);
+    rbuf->content.taskid = task.taskid;
+    taskid_be = htobe64(task.taskid);
+    lseek(fd, 0, SEEK_SET);
+    write(fd, &taskid_be, sizeof(uint64_t));
     close(fd);
+    return 0;
+}
 
-    if (cbuf->type == SI_TYPE) {
-        snprintf(path, strlen(filename) + 6, "%s/argv", filename);
-        fd = open(path, O_RDONLY);
-        if (fd < 0) return 1;
-        int ret = readarguments(fd, &(cbuf->content.args));
-        close(fd);
-        return ret;
-    } else if (cbuf->type == SQ_TYPE) {
-        struct dirent *entry;
-        cbuf->content.combined.nbcmds = 0;
-        char **names = NULL;
-
-        DIR *dirp = opendir(filename);
-        if (!dirp) return 1;
-
-        while ((entry = readdir(dirp))) {
-            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 && (entry->d_type == DT_DIR)) {
-                names = realloc(names, (cbuf->content.combined.nbcmds + 1) * sizeof(char *));
-                names[cbuf->content.combined.nbcmds++] = (entry->d_name);
-            }
+int handle_combine_request(struct request req, struct reply *rbuf) {
+    uint64_t taskid_be = 0;
+    int fd = open("last-taskid", O_RDWR | O_CREAT, 0644);
+    struct stat st;
+    if (fstat(fd, &st) < 0)
+        return 1;
+    if (st.st_size != 0) {
+        read(fd, &taskid_be, sizeof(uint64_t));
+    }
+    char path_tasksid[req.content.cr.content.combined.nbtasks][PATH_MAX];
+    for (uint64_t i = 0; i < req.content.cr.content.combined.nbtasks; i++) {
+        snprintf(path_tasksid[i], sizeof(path_tasksid), "tasks/%zu/", req.content.cr.content.combined.tasksid[i]);
+        if (!access(path_tasksid[i], F_OK) == 0) {
+            rbuf->anstype = ER_ANSTYPE;
+            rbuf->content.errcode = NF_ERRCODE;
+            return 0;
         }
+    }
+    struct task task = {be64toh(taskid_be) + 1, req.content.cr.task_timing, {req.content.cr.content.combined.type, {}}};
+    char path_task[PATH_MAX];
+    snprintf(path_task, sizeof(path_task), "tasks/%zu/", task.taskid);
+    mkdirtask(path_task, &task);
+    rbuf->content.taskid = task.taskid;
+    taskid_be = htobe64(task.taskid);
+    strcat(path_task, "cmd/");
+    int len_path_task = strlen(path_task);
+    for (uint64_t i = 0; i < req.content.cr.content.combined.nbtasks; i++) {
+        int len_path_tasksid = strlen(path_tasksid[i]);
+        strcat(path_tasksid[i], "cmd/");
+        snprintf(path_task + len_path_task, sizeof(path_tasksid[i]) - len_path_task, "%zu", i);
+        rename(path_tasksid[i], path_task);
+        path_tasksid[i][len_path_tasksid] = '\0';
+        rmdirtask(path_tasksid[i]);
+    }
+    ftruncate(fd, 0);
+    lseek(fd, 0, SEEK_SET);
+    write(fd, &taskid_be, sizeof(uint64_t));
+    close(fd);
+    return 0;
+}
 
-        if (cbuf->content.combined.nbcmds == 0) return 1;
-        cbuf->content.combined.cmds = malloc(cbuf->content.combined.nbcmds * sizeof(struct command));
-        if (!cbuf->content.combined.cmds) return 1;
-        insertion_sort(names, cbuf->content.combined.nbcmds);
+int handle_remove_request(struct request req, struct reply *rbuf) {
+    char path_task[PATH_MAX];
+    snprintf(path_task, sizeof(path_task), "task/%zu", req.content.taskid);
+    // TODO fonction qui supprime une tache dans task.h et renvoie si la supreesion à échoué ou non
+    return 0;
+}
 
-        for (int i = 0; i < cbuf->content.combined.nbcmds; i++) {
-            snprintf(path, strlen(filename) + strlen(names[i]) + 2, "%s/%s", filename, names[i]);
-            if (readcmd_path(path, (cbuf->content.combined.cmds) + i) != 0) {
-                free(names);
-                closedir(dirp);
-                return 1;
-            }
-        }
-        free(names);
-        closedir(dirp);
+int handle_times_exitcodes_request(struct request req, struct reply *rbuf) {
+    struct stat st;
+    char path_tec[PATH_MAX];
+    snprintf(path_tec, sizeof(path_tec), "tasks/%zu/", req.content.taskid);
+    size_t len = strlen(path_tec);
+    if (stat(path_tec, &st) < 0) {
+        rbuf->anstype = ER_ANSTYPE;
+        rbuf->content.errcode = NF_ERRCODE;
         return 0;
     }
+    snprintf(path_tec + len, sizeof(path_tec) - len, "times-exitcodes");
+    rbuf->anstype = OK_ANSTYPE;
+    int fd = open(path_tec, O_RDONLY);
+
+    if (fd < 0) {
+        perror("Erreur open");
+        return 1; 
+    }
+
+    if (fstat(fd, &st) < 0) {
+        perror("erreur fstat");
+        return 1;
+    }
+    rbuf->content.tec.nbruns = 0;
+    read_times_exitcodes(fd, &(rbuf->content.tec));
     return 0;
 }
 
-int readcmd_fd(int fd, struct command *cbuf) {
-    uint16_t type_be;
-    if (read(fd, &type_be, sizeof(uint16_t))!= sizeof(uint16_t)) return 1;
-    cbuf->type = be16toh(type_be);
-    if (cbuf->type== SI_TYPE) {
-        if (readarguments(fd, &(cbuf->content.args)) == 1) return 1;
-    } else if (cbuf->type == SQ_TYPE) {
-        uint32_t nbcmds_be;
-        if (read(fd, &nbcmds_be, sizeof(uint32_t))!= sizeof(uint32_t)) return 1;
-        cbuf->content.combined.nbcmds = be32toh(nbcmds_be);
-        cbuf->content.combined.cmds = malloc(cbuf->content.combined.nbcmds * sizeof(struct command));
-
-        for (int i = 0; i < cbuf->content.combined.nbcmds; i++) {
-            if (readcmd_fd(fd, cbuf->content.combined.cmds + i) == 1) return 1;
-        }
+int handle_std_request(struct request req, struct reply *rbuf) {
+    struct stat st;
+    char path_std[PATH_MAX];
+    snprintf(path_std, sizeof(path_std), "tasks/%zu/", req.content.taskid);
+    size_t len = strlen(path_std);
+    if (stat(path_std, &st) < 0) {
+        rbuf->anstype = ER_ANSTYPE;
+        rbuf->content.errcode = NF_ERRCODE;
+        return 0;
     }
-    return 0;
-}
-
-int writecmd(int fd, struct command *cbuf) {
-    uint16_t type_be = htobe16(cbuf->type);
-    if (write(fd, &type_be, sizeof(uint16_t))!= sizeof(uint16_t)) return 1;
-    if (cbuf->type == SI_TYPE) {
-        if (writearguments(fd, &(cbuf->content.args)) == 1) return 1;
-    } else if (cbuf->type == SQ_TYPE) {
-        uint32_t nbcmds_be = htobe32(cbuf->content.combined.nbcmds);
-        if (write(fd, &nbcmds_be, sizeof(uint32_t)) !=sizeof(uint32_t)) return 1;
-        for (int i = 0; i < cbuf->content.combined.nbcmds; i++) {
-            if (writecmd(fd, cbuf->content.combined.cmds + i) == 1) return 1;
-        }
+    if (req.opcode == SO_OPCODE) {
+        snprintf(path_std + len, sizeof(path_std) - len, "stdout");
+        if (stat(path_std, &st) < 0) {
+            rbuf->anstype = ER_ANSTYPE;
+            rbuf->content.errcode = NR_ERRCODE;
+            return 0;
     }
-    return 0;
-}
-
-void freecmd(struct command *cbuf) {
-    if (cbuf->type == SI_TYPE) {
-        freearguments(&(cbuf->content.args));
-    } else if (cbuf->type == SQ_TYPE) {
-        uint32_t host_nbcmds = cbuf->content.combined.nbcmds;
-        for(int i = 0; i < host_nbcmds; i++) {
-            freecmd(&(cbuf->content.combined.cmds[i]));
-        }
-        free(cbuf->content.combined.cmds);
-    }
-}
-
-
-uint16_t executecmd(struct command *cbuf) {
-    pid_t p = fork();
-    if (p < 0) {
-        exit(1);
-    } else if (p == 0) {
-        if (cbuf->type == SI_TYPE) {
-            exit(executearg(&cbuf->content.args));
-        } else if (cbuf->type == SQ_TYPE) {
-            uint16_t finalexit = 0;
-            uint32_t nbcmds_host = cbuf->content.combined.nbcmds;
-            for (uint32_t i = 0; i < nbcmds_host; i++) {
-                finalexit = executecmd(cbuf->content.combined.cmds + i);
-            }
-            exit(finalexit);
-        }
-        exit(1);
     } else {
-        int status;
-        wait(&status);
-        if (WIFEXITED(status)) {
-            return (uint16_t) WEXITSTATUS(status);
+        snprintf(path_std + len, sizeof(path_std) - len, "stderr");
+        if (stat(path_std, &st) < 0) {
+            rbuf->anstype = ER_ANSTYPE;
+            rbuf->content.errcode = NR_ERRCODE;
+            return 0;
         }
-        return 0xffff;
     }
+    rbuf->anstype = OK_ANSTYPE;
+    int fd = open(path_std, O_RDONLY);
+    readstd(fd, &(rbuf->content.output));
+    return 0;
 }
 
-int command_to_string(struct command c,struct string *s) {
-    if (c.type == SI_TYPE) {
-        if (arguments_to_string(c.content.args, s) == 1) return 1;
-    } else {
-        struct string start = {1, (uint8_t*) "("};
-        struct string space = {1, (uint8_t*) " "};
-        struct string semicolon = {3, (uint8_t*) " ; "};
-        struct string end = {1, (uint8_t*) ")"};
-
-        for (int i = 0; i< c.content.combined.nbcmds; i++) {
-            if (c.content.combined.cmds[i].type == SQ_TYPE) {
-                if (catstring(s, start) == 1) return 1;
-            }
-            catstring(s, space);
-            if (command_to_string(c.content.combined.cmds[i], s) == 1) return 1;
-            catstring(s, space);
-            if (c.content.combined.cmds[i].type == SQ_TYPE) {
-                if (catstring(s, end) == 1) return 1;
-            }
-            if (i != c.content.combined.nbcmds-1) {
-                if (catstring(s, semicolon) == 1) return 1;
-            }
-        }
+int handle_request(struct request req, struct reply *rep) {
+    switch (req.opcode) {
+        case LS_OPCODE :
+            handle_list_request(req, rep);
+            break;
+        case CR_OPCODE :
+            handle_creat_request(req, rep);
+            break;
+        case CB_OPCODE :
+            handle_combine_request(req, rep);
+            break;
+        case RM_OPCODE :
+            handle_remove_request(req, rep);
+            break;
+        case TX_OPCODE :
+            handle_times_exitcodes_request(req, rep);
+            break;
+        case SO_OPCODE :
+        case SE_OPCODE :
+            handle_std_request(req, rep);  
+            break;
+        case TM_OPCODE :
+            rep->anstype = OK_ANSTYPE;
+            break;
     }
     return 0;
+}
+
+int handle_reply(struct reply rep, uint16_t opcode, struct string *msg) {
+    int ret = 0;
+    struct string rtl = {1, (uint8_t*) "\n"};
+    if (rep.anstype == OK_ANSTYPE) {
+        switch (opcode) {
+            case LS_OPCODE :
+                for (int i = 0; i < rep.content.list.nbtasks; i++) {
+                    task_to_string(rep.content.list.tasks[i], msg);
+                    catstring(msg, rtl);
+                }
+                break;
+            case CR_OPCODE :
+            case CB_OPCODE :
+                uint_to_string(rep.content.taskid, msg);
+                catstring(msg, rtl);
+                break;
+            case RM_OPCODE :
+                break;
+            case TX_OPCODE :
+                times_exitcodes_to_string(rep.content.tec, msg);
+                break;
+            case SO_OPCODE :
+            case SE_OPCODE :
+                catstring(msg, rep.content.output);
+                break;
+            case TM_OPCODE :
+                break;
+        }
+    } else {
+        ret = 1;
+    }   
+    return ret;
 }
